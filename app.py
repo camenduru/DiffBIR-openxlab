750ed8deec2bd160a774708e07646bf5d766e837
camenduru <54370274+camenduru@users.noreply.github.com>
2023-09-11 12:52:26 +0300

    test

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+os.chdir(f"/home/xlab-app-center")
+os.system(f"git clone -b openxlab https://github.com/camenduru/DiffBIR /home/xlab-app-center/DiffBIR")
+os.chdir(f"/home/xlab-app-center/DiffBIR")
+os.system(f"git lfs install")
+os.system(f"git pull")
+os.system(f"git reset --hard")
+os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/camenduru/DiffBIR/resolve/main/general_full_v1.ckpt -d /home/xlab-app-center/models -o general_full_v1.ckpt")
+os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/camenduru/DiffBIR/resolve/main/general_swinir_v1.ckpt -d /home/xlab-app-center/models -o general_swinir_v1.ckpt")
+os.system(f"python gradio_diffbir.py --ckpt /home/xlab-app-center/models/general_full_v1.ckpt --config /home/xlab-app-center/DiffBIR/configs/model/cldm.yaml --reload_swinir --swinir_ckpt /home/xlab-app-center/models/general_swinir_v1.ckpt")
